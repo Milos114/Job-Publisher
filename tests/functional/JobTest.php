2297396b43e9cd9e6a02f8e85207f3bcecdfd739
Milos Stefanovic <milos.stefanovic@cosmicdevelopment.com>
2016-11-18 13:11:49 +0100

    JobTests

--- tests/functional/JobTest.php
+++ tests/functional/JobTest.php
@@ -10,6 +10,36 @@ class JobTest extends TestCase
     use DatabaseTransactions;
 
     /** @test */
+    public function it_confirms_job_submission_page_exists()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $response = $this->call('GET', 'job-submission');
+
+        $this->assertEquals(200, $response->status());
+    }
+
+    /** @test */
+    public function it_confirms_job_submission_page_has_tags_variable()
+    {
+        $user = factory(User::class)->create();
+        $this->actingAs($user);
+
+        $this->call('GET', 'job-submission');
+
+        $this->assertViewHas('tags');
+    }
+
+    /** @test */
+    public function it_redirects_if_user_is_not_authenticated()
+    {
+        $this->call('GET', 'job-submission');
+
+        $this->assertRedirectedTo('login');
+    }
+
+    /** @test */
     public function it_submits_given_user_jobs()
     {
         $user = factory(User::class)->create();
